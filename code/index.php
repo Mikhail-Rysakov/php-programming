b6770149c832f488ea92f032862f776efcb2dafe
Mikhail-Rysakov <98999701+Mikhail-Rysakov@users.noreply.github.com>
2022-02-06 18:32:25 +0200

    Update index.php
    Task 2

--- code/index.php
+++ code/index.php
@@ -6,5 +6,16 @@ $very_bad_unclear_name = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= "redro";
 echo "$order";   //same as "very_bad_unclear_name"
+
     
+//PART2: numbers
+echo "\n\nTASK2 \n";
+$numInt = 9;
+echo "{$numInt} \n";
+$numFloat = 9.0453;
+echo "{$numFloat} \n";
+echo "12 \n";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month-$this_month;
 ?>
