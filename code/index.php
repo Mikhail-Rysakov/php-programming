33a852a1334738929e3397cc1c2d15ab2ae42261
Mikhail-Rysakov <98999701+Mikhail-Rysakov@users.noreply.github.com>
2022-02-06 18:20:59 +0200

    Update index.php
    Task 1

--- code/index.php
+++ code/index.php
@@ -1,2 +1,10 @@
-<?php
-echo phpinfo();
+<?php //php 7.2.24
+
+//PART1: access by reference
+echo "TASK1 \n";    
+$very_bad_unclear_name = "15 chicken wings";
+$order = &$very_bad_unclear_name;
+$order .= "redro";
+echo "$order";   //same as "very_bad_unclear_name"
+    
+?>
