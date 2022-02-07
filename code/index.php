4e96110c208271e1866e4e58fe37db63073d1493
Mikhail-Rysakov <98999701+Mikhail-Rysakov@users.noreply.github.com>
2022-02-07 17:12:41 +0200

    Update index.php
    made it more structurized and readable.

--- code/index.php
+++ code/index.php
@@ -1,38 +1,46 @@
 <?php //php 7.2.24
 
-//PART1: access by reference
-echo "TASK1 \n";    
+//Chapter1: Access by reference
+echo "\n\n\nChapter1: Access by reference\n";   
+//    
 $very_bad_unclear_name = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= "redro";
-echo "$order";   //same as "very_bad_unclear_name"
+echo "\t$order";   //same as "very_bad_unclear_name"
 
-    
-//PART2: numbers
-echo "\n\nTASK2 \n";
+
+//Chapter2: Numbers  
+echo "\n\n\nChapter2: Numbers";                  
+//
 $numInt = 9;
-echo "{$numInt} \n";
+echo "\n\t" . $numInt;
 $numFloat = 9.0453;
-echo "{$numFloat} \n";
-echo "12 \n";
+echo "\n\t" . $numFloat;
+echo "\n\t12";
 $last_month = 1187.23;
 $this_month = 1089.98;
-echo $last_month-$this_month;
+echo "\n\t" . $last_month-$this_month;
 
-//PART3: multiplying and division
-echo "\n\nTASK3 \n";
+
+//Chapter3: Multiplying and division
+echo "\n\n\nChapter3: Multiplying and division"; 
+//
 $num_languages = 4;
 $months = 11;
 $days = $months * 16;
 $days_per_language = $days / $num_languages;
-echo $days_per_language;
+echo "\n\t" . $days_per_language;
 
-//PART4: degree
-echo "\n\nTASK4 \n";
-echo 8**2;
 
-//PART5: assignment
-echo "\n\nTASK5 \n";
+//Chapter4: Degree
+echo "\n\n\nChapter4: Degree";                     
+//
+echo "\n\t" . 8**2;
+
+
+//Chapter5: Assignment
+echo "\n\n\nChapter5: Assignment";                 
+//
 $my_num = 9;
 $answer = $my_num;
 $answer += 2;
@@ -40,6 +48,6 @@ $answer *= 2;
 $answer -= 2;
 $answer /= 2;
 $answer -= $my_num;
-echo $answer;
+echo "\n\t" . $answer;
 
 ?>
