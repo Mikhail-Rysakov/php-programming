3a322a0bd7a26b666e41622719adecbbdf46a34a
Mikhail-Rysakov <98999701+Mikhail-Rysakov@users.noreply.github.com>
2022-02-25 15:53:23 +0200

    little renaming

--- 3_Lab/1_Task/Task1.php
+++ 3_Lab/1_Task/Task1.php
@@ -1,7 +1,7 @@
 <?php //php 7.2.24
 
 ////////////////////////////
-//Part1: Finding a word in the phrase
+//Part A: Finding a word in the phrase
 $phrase = "ahb acb aeb aeeb adcb axeb";
 $mask = "/a[a-z0-9]{2}b/";
 $matches = [];
@@ -10,7 +10,7 @@ print_r($matches);
 
 
 ////////////////////////////
-//Part2: Conversion of numbers
+//Part B: Conversion of numbers
 $expression = "a1b2c3";
 echo preg_replace_callback('/[0-9]/', function ($expression) {   //Использование лямбда-функции (анонимной-функции)
     return strtoupper($expression[0] ** 3);}, $expression);
